0bef185a556a4c5a066f10582ca08996eac329a0
Mykhailo Pashko <pshk@Mykhailos-MacBook-Pro.local>
2021-02-03 22:16:09 +0200

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+import os, sys
+
+
+class Article(models.Model):
+    article_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    published = models.DateTimeField(default=timezone.now)
+
+
+class Group(models.Model):
+    group_id = models.AutoField(primary_key=True)
+    articles = models.ManyToManyField(Article)
+
+
+
+class someUglyClass(models.Model):
+    """Extra long docstring --------------------------------------------------------"""
+    text = models.TextField()
+
+    def Method_A(self):
+        return os.environ.get('a')
+
+    def method_b(self):
+        return sys.argv
